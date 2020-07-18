18afe37ebb9f467619e309ffa124ea42934d2d2c
Agung perdana <50897936+calvinsp@users.noreply.github.com>
2020-07-18 17:50:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function(){
     nav.classList.toggle('muncul');
 });
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
